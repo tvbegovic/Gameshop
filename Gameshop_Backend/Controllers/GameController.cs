67ecad3a64f22e2fbdd35d5e974369b3e3afc2d1
Tvrtko Begović <tvbegovic@adamant69.onmicrosoft.com>
2020-12-18 15:46:53 +0100

    gamecontroller nove funkcije, Include

--- Gameshop_Backend/Controllers/GameController.cs
+++ Gameshop_Backend/Controllers/GameController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Gameshop_Backend.Db;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gameshop_Backend.Controllers
 {
@@ -19,10 +20,35 @@ namespace Gameshop_Backend.Controllers
 			this.context = context;
 		}
 
-		[HttpGet]
+		[HttpGet("genres")]
 		public List<Genre> GetGenres()
 		{
 			return context.Genres.ToList();
+		}
+
+		[HttpGet("companies")]
+		public List<Company> GetCompanies()
+		{
+			return context.Companies.ToList();
+		}
+
+		[HttpGet("bygenre/{id:int}")]
+		public List<Game> GetByGenre(int id)
+		{
+			return context.Games.Where(g => g.IdGenre == id).ToList();
+		}
+		
+		[HttpGet("bycompany/{id:int}")]
+		public List<Game> GetByCompany(int id)
+		{
+			return context.Games.Where(g => g.IdDeveloper == id || g.IdPublisher == id).ToList();
+		}
+
+		[HttpGet("search/{text}")]
+		public List<Game> Search(string text)
+		{
+			return context.Games.Include(g => g.Genre).Include(g => g.Publisher)
+				.Where(g => g.Title.Contains(text) || g.Genre.Name.Contains(text) || g.Publisher.Name.Contains(text)).ToList();
 		}
 	}
 }
